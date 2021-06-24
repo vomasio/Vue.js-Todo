ad61a66254bc9e013380fca58f8b934b2c13aca9
vomasio <zakicit@yahoo.co.jp>
2021-06-24 02:56:37 +0000

    STEP12

--- main.js
+++ main.js
@@ -1,63 +1,76 @@
 // https://jp.vuejs.org/v2/examples/todomvc.html
-var STORAGE_KEY = 'todos-vuejs-demo'
+var STORAGE_KEY = 'todos-vuejs-demo';
 var todoStorage = {
   fetch: function() {
     var todos = JSON.parse(
       localStorage.getItem(STORAGE_KEY) || '[]'
-    )
+    );
     todos.forEach(function(todo, index) {
-      todo.id = index
-    })
-    todoStorage.uid = todos.length
-    return todos
+      todo.id = index;
+    });
+    todoStorage.uid = todos.length;
+    return todos;
   },
   save: function(todos) {
-    localStorage.setItem(STORAGE_KEY, JSON.stringify(todos))
+    localStorage.setItem(STORAGE_KEY, JSON.stringify(todos));
   }
-}
+};
 const app = new Vue({
     el: '#app',
     data: {
         // use data
-        todos: []
+        todos: [],
+        options: [
+            {value: -1, label: 'All'},
+            {value: 0, label: 'Do'},
+            {value: 1, label: 'Complete'}
+        ],
+        current: -1
     },
     methods: {
         //use method
         doAdd: function(event, value) {
             //element that named (ref)
-            var comment = this.$refs.comment
+            var comment = this.$refs.comment;
             //when no input
             if(!comment.value.length) {
-                return
+                return;
             }
             //{newID,comment,status} push for todos
             this.todos.push({
                 id: todoStorage.uid++,
                 comment: comment.value,
                 state: 0
-            })
-            comment.value = ''
+            });
+            comment.value = '';
         },
         //Change State
         doChangeState: function(item) {
-            item.state = item.state ? 0 : 1
+            item.state = item.state ? 0 : 1;
         },
         //Delete
         doRemove: function(item) {
-            var index = this.todos.indexOf(item)
-            this.todos.splice(index, 1)
+            var index = this.todos.indexOf(item);
+            this.todos.splice(index, 1);
         }
     },
     // "watch" for auto save
     watch: {
         todos: {
             handler: function(todos) {
-                todoStorage.save(todos)
+                todoStorage.save(todos);
             },
             deep: true
         }
     },
+    computed: {
+        computedTodos: function() {
+            return this.todos.filter(function(el) {
+                return this.current <0 ? true : this.current === el.state;
+            }, this);
+        }
+    },
     created() {
-        this.todos = todoStorage.fetch()
+        this.todos = todoStorage.fetch();
     }
-})
+});
